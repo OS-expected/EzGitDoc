30fb91c88f2f20e905fef7a0a97bcf0740c62a82
Paweł Idzikowski <iddziku@gmail.com>
2020-02-24 14:54:24 +0100

    First steps on logo

--- js/ezOther.js
+++ js/ezOther.js
@@ -5,4 +5,15 @@ window.addEventListener('load', function () {
 
     var imageAlign = document.getElementById('imagePositionList');
     imageAlign.value = 0;
+
+    var logo = document.getElementById('ezLogo');
+    logo.classList.add('faa-bounce', 'animated');
+
+    setTimeout(removeLogoAnimation, 2000);
+
+    function removeLogoAnimation()
+    {
+        logo.classList.remove('faa-bounce', 'animated');
+    }
 })
+
--- js/ezWebLogic.js
+++ js/ezWebLogic.js
@@ -10,7 +10,7 @@ var wrongImageExtension_part1 = 'Woops! Seems like you tried to reference to ima
 var wrongImageExtension_part2 = '.Supported extensions for now are: jpg, jpeg, bmp, gif, png';
 
 // ********************************************
-// EzGitDoc CREATE Logic
+// EzGitDoc Creating Elements Logic
 // ********************************************
 
 function createHeader() {
@@ -197,6 +197,33 @@ function createImage()
 
     workingSpace.appendChild(paragraph);
 }
+
+function createTable() {
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < 3; i++) {
+      var tr = document.createElement('tr');
+      for (var j = 0; j < 2; j++) {
+        if (i == 2 && j == 1) {
+          break
+        } else {
+          var td = document.createElement('td');
+          td.appendChild(document.createTextNode('\u0020'))
+          i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+          tr.appendChild(td)
+        }
+      }
+      tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl)
+}
+
+// ********************************************
+// EzGitDoc Other
+// ********************************************
 
 function removeElementByParentId(elementId) {
     var element = document.getElementById(elementId.parentNode.id);
