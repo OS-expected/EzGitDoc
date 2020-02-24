4901db64c386dbf16ee7120137215da63e1aabfa
Paweł Idzikowski <iddziku@gmail.com>
2020-02-24 12:01:36 +0100

    Further work on image, modify header creation

--- js/ezOther.js
+++ js/ezOther.js
@@ -2,4 +2,7 @@
 window.addEventListener('load', function () {
     var style = document.getElementById('headerStyleList');
     style.value = 0;
+
+    var imageAlign = document.getElementById('imagePositionList');
+    imageAlign.value = 0;
 })
