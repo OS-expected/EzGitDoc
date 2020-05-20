022e4c5a21a2b8223726f8375605ad26371359be
Paweł Idzikowski <iddziku@gmail.com>
2020-05-20 12:15:06 +0200

    Implement image and list generation

--- js/ezQuickBuilder.js
+++ js/ezQuickBuilder.js
@@ -42,15 +42,65 @@ function generateQuickTemplate() {
             }
             quickBuilderOutput += '<' + headerVal.toLowerCase() + '> Heading </' + headerVal.toLowerCase() + '>';
         } else if(command.startsWith('image')) {
-            
+            var imgVal = command.substring(
+                command.lastIndexOf("{") + 1, 
+                command.lastIndexOf("}")
+            );
+            if(imgVal <= 0) {
+                unrecognizedCommands += ' (' + commandNum + ')'; 
+                continue;
+            } else if(imgVal > 0) {
+                quickBuilderOutput += '<img src="http://placehold.it/"' + imgVal + '/>';
+            }
         } else if(command.startsWith('link')) {
-            
+            quickBuilderOutput += '<a href="fill.in.address">link</a>"';
         } else if(command.startsWith('list')) {
-            
+            var type = command.substring(
+                command.lastIndexOf("{") + 1, 
+                command.lastIndexOf("-")
+            );
+            var amount = command.substring(
+                command.lastIndexOf("-") + 1, 
+                command.lastIndexOf("}")
+            );
+            console.log('type => ' + type);
+            console.log('amount => ' + amount);
+
+            if(amount <= 0) {
+                unrecognizedCommands += ' (' + commandNum + ')'; 
+                continue;
+            }
+
+            if(type == 'iconic') {
+                // :pirn2odiy: <strong>bold text:</strong> description
+                for(z = 0; z < amount; z++) {
+                    quickBuilderOutput += ':pirn2odiy: <strong>bold text:</strong> description';
+                    if((z + 1) != amount) {
+                        quickBuilderOutput += '\r\n';
+                    }
+                }
+            } else if (type == 'normal') {
+                quickBuilderOutput += '<ul>\r\n';
+                // <li>element</li>
+                for(z = 0; z < amount; z++) {
+                    quickBuilderOutput += '<li>element</li>\r\n';
+                }
+                quickBuilderOutput += '</ul>';
+            } else if (type == 'link') {
+                quickBuilderOutput += '<ul>\r\n';
+                // <li><a href="fill.in.address">link</a></li>
+                for(z = 0; z < amount; z++) {
+                    quickBuilderOutput += '<li><a href="fill.in.address">link</a></li>\r\n';
+                }
+                quickBuilderOutput += '</ul>';
+            } else {
+                unrecognizedCommands += ' (' + commandNum + ')'; 
+                continue;
+            }       
         } else if(command.startsWith('table')) {
             
         } else if(command.startsWith('text')) {
-            
+            quickBuilderOutput += '<p align="center|left|right">text</p>';
         } else if(command.startsWith('label')) {
             
         } else {
@@ -91,7 +141,7 @@ function insertLinkTemplate() {
 }
 
 function insertListTemplate() {
-    insertAtCursor(' list{type}{amount}');
+    insertAtCursor(' list{type-amount}');
 }
 
 function insertTableTemplate() {
